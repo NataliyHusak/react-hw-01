1477f60a7e971893f584ece5b1693f4553e247ef
Nataliy Husak <nataliy.husak@gmail.com>
2023-04-05 17:52:27 +0300

    app

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
+import BrowserRouter from 'react-router-dom';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
+
+  <BrowserRouter basename="/react-hw-01">
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
